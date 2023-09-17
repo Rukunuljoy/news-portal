1aadc82b3d132f9b89b091be75a5c6007ef2ab68
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-09-17 23:55:11 +0600

    create a news portal project with Reactjs

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Head = () => {
+    return (
+        <section className="head">
+            <div className="container flexSB paddingTB">
+                <div className="logo">
+                    <img src="/images/logo.png" alt="" />
+                </div>
+                <div className="ad">
+                    <img src="../images/headerb.png" alt="" />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Head;
---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,12 @@
+import Head from "./Head";
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <Head/>
+        </div>
+    );
+};
+
+export default Header;

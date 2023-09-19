86595878ff2b4d57f8f7f3508b48996f8574fda1
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-09-19 13:16:05 +0600

    popular is complete

--- src/components/home/mainContent/popular/Polpular.jsx
+++ src/components/home/mainContent/popular/Polpular.jsx
@@ -1,43 +1,70 @@
-import { popular } from '../../../../../fakeData';
-import Heading from '../../../common/Heading/Heading';
-import './Popular.css'
-import {FaCalendar, FaComment} from 'react-icons/fa'
+import Slider from "react-slick";
+import { popular } from "../../../../../fakeData";
+import Heading from "../../../common/Heading/Heading";
+import "./Popular.css";
+import { FaCalendar, FaComment } from "react-icons/fa";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Polpular = () => {
-    return (
-        <section className='popular'>
-            <Heading title="Popular"/>
-            {
-                popular.map((val)=>{
-                    return (
-                    <div key={val.id} className="items">
-                        <div className="box shadow">
-                            <div className="images row">
-                                <div className="img">
-                                    <img src={val.cover} alt="" />
-                                </div>
-                                <div className="category category1">
-                                    <span>{val.category}</span>
-                                </div>
-                            </div>
-                            <div className="text ro">
-                                <h1 className="title">{val.title.slice(0,40)}...</h1>
-                                <div className="date">
-                                    <FaCalendar/>
-                                    <label htmlFor="">{val.date}</label>
-                                </div>
-                                <div className="comment">
-                                    <FaComment/>
-                                    <label htmlFor="">{val.date}</label>
-                                </div>
-                            </div>
-                        </div>
+    const settings = {
+        className: "center",
+        centerMode: false,
+        infinite: true,
+        centerPadding: "0",
+        slidesToShow: 2,
+        speed: 500,
+        rows: 4,
+        slidesPerRow: 1,
+        responsive: [
+          {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              rows: 4,
+            },
+          },
+        ],
+      }
+  return (
+    <>
+       <section className='popular'>
+        <Heading title='Popular' />
+        <div className='content'>
+          <Slider {...settings}>
+            {popular.map((val) => {
+              return (
+                <div key={val.id} className='items'>
+                  <div className='box shadow'>
+                    <div className='images row'>
+                      <div className='img'>
+                        <img src={val.cover} alt='' />
+                      </div>
+                      <div className='category category1'>
+                        <span>{val.category}</span>
+                      </div>
                     </div>
-        )
-        })
-    }
-        </section>
-    );
+                    <div className='text row'>
+                      <h1 className='title'>{val.title.slice(0, 40)}...</h1>
+                      <div className='date'>
+                        <FaCalendar className="calender"/>
+                        <label>{val.date}</label>
+                      </div>
+                      <div className='comment'>
+                        <FaComment className="calender"/>
+                        <label>{val.comments}</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </Slider>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Polpular;
